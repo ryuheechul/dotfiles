4277aa68b5235cb84fd194a4b6902141b8054a90
Heechul Ryu <ryuhcii@gmail.com>
2017-12-22 16:53:28 +0900

    Bump lein's cider nrepl to 0.16.0

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:repl {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
+{:repl {:plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]
                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[alembic "0.3.2"]
                        [org.clojure/tools.nrepl "0.2.12"]]}}
