fbce7d24fa2588d5f02ed66cf879284890d21723
Heechul Ryu <ryuhcii@gmail.com>
2020-02-19 22:27:40 +0000

    Preview markdown with glow

--- lf/pv.sh
+++ lf/pv.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
 unset COLORTERM
-#bat --color=always "$@"
-bat --color=always --theme=base16 "$@"
 
+filename=$(basename $1)
+EXT="${filename##*.}"
+
+# lowercasing
+# if this doesn't work check if you bash is old like 3.x on like on macOS
+# then `$ brew install bash`
+ext="${EXT,,}"
+
+if [ $ext == "md" ]; then
+  echo '`# Markdown: '"$1"'`via **glow**' | glow - -s dark
+  glow -s dark "$1"
+else
+  bat --color=always --theme=base16 "$@"
+fi
