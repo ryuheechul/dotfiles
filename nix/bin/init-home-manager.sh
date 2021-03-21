dd26fa8e18a66b3b1e51fc8bad90236a7af91ab0
Heechul Ryu <ryuhcii@gmail.com>
2021-03-21 10:13:13 +0000

    Fix imports for ~/.config/nixpkgs/home.nix

--- nix/bin/init-home-manager.sh
+++ nix/bin/init-home-manager.sh
@@ -2,20 +2,24 @@
 
 set -x
 
+curr_dir="$(dirname "$0")"
+repo_root="${curr_dir}/../.."
+repo_root_abs="$(readlink -f "${repo_root}")"
+nix_home_path="${repo_root_abs}/nix/home.nix"
+
 nix-shell '<home-manager>' -A install
 
 # use this as a template to replace ~/.config/nixpkgs/home.nix
-
 cat << EOF > ~/.config/nixpkgs/home.nix
 let
   imports = [
-    ~/dotfiles/nix/home.nix
+    $(echo "${nix_home_path}")
   ];
 in
 {
   inherit imports;
 
   home.username = "$(whoami)";
-  home.homeDirectory = "$(echo $HOME)";
+  home.homeDirectory = "$(echo "${HOME}")";
 }
 EOF
