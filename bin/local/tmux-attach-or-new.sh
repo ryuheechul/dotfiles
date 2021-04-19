88ff1ff1d2663d40a0d440d7b969d57fbeb3e7c9
Heechul Ryu <ryuhcii@gmail.com>
2021-04-19 21:05:41 +0100

    tmux - attach scripts works better in different environments

--- bin/local/tmux-attach-or-new.sh
+++ bin/local/tmux-attach-or-new.sh
@@ -4,6 +4,11 @@ if [[ -z "$1" ]]; then
   echo "you will need to provide a session name. Try \`$0 [my-session-name]\`."
   exit 1
 fi
+
+# enable nix for the rest of script to prevent using system's old tmux
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+SHELL=$(which zsh)
 session_name="$1"
 # recommended via https://stackoverflow.com/a/3432749
 tmux a -t "${session_name}" || tmux new -s "${session_name}"
