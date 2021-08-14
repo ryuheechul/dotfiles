8963625b9cb6c36864f4e503aafdfe66878cb1e2
Heechul Ryu <ryuhcii@gmail.com>
2021-08-14 13:40:30 +0100

    tmux - fix `fpp` unavailable issue

--- bin/local/tmux-attach-or-new.sh
+++ bin/local/tmux-attach-or-new.sh
@@ -5,17 +5,13 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
+# set path for nix otherwise tmux server can't access some binaries like `fpp`
+. ~/.nix-profile/etc/profile.d/nix.sh
+
 # let brew (binaries) takes more priority then nix in case of apple silicon
 if test "Darwin" = "$(uname)" && test "arm64" = "$(arch)"; then
   [ -x /opt/homebrew/bin/brew ] && \
     eval $(/opt/homebrew/bin/brew shellenv)
-fi
-
-# try nix when brew doesn't provide zsh or tmux
-if test -z "$(command -v brew)" || \
-  test -z "$(command -v zsh)" || \
-  test -z "$(command -v tmux)"; then
-  . ~/.nix-profile/etc/profile.d/nix.sh
 fi
 
 SHELL=$(which zsh)
