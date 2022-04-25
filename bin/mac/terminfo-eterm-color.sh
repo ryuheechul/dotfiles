2a51530b910a4e1ea78221c6afc343bb51ecaf1c
Heechul Ryu <ryuhcii@gmail.com>
2022-04-25 11:03:19 -0700

    bin/local/nix-outpath - fix the script to be more portable

--- bin/mac/terminfo-eterm-color.sh
+++ bin/mac/terminfo-eterm-color.sh
@@ -2,3 +2,6 @@
 
 # might want to run this in case one from ncurses is not satisfying
 /usr/bin/tic -x "$(nix-outpath emacs)/share/emacs/28.1/etc/e/eterm-color.ti"
+
+echo 'A "proper" `eterm-color` terminfo on this system (darwin) was missing and now it is installed by this script.'
+echo 'Restart the shell/terminal to take effect'
