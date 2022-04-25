6d57f886436c88fee42ecc5514e7324252f81b30
Heechul Ryu <ryuhcii@gmail.com>
2022-04-25 00:22:52 -0700

    neovim - watch base16-shell theme change
    and fix its tone accordingly

--- nvim/lua/packer-groups/theme.lua
+++ nvim/lua/packer-groups/theme.lua
@@ -2,22 +2,45 @@
 
 -- because of the nature of switching color schems, there will be unused ones listed but commented
 
+-- this function seems to be global in order to be called from below
+-- I could wrap the whole thing in a function to have a closure effect
+-- but I'm lazy and why not have this function to be available in command line as well
+function Follow_base16_shell()
+  -- instead of relying vim.env.BASE16_THEME
+  -- `current-base16` response the most accurate value
+  local handle = io.popen 'current-base16'
+  local result = handle:read '*a'
+  local theme_base = (result:gsub('solarized--', ''))
+  handle:close()
+
+  vim.api.nvim_set_option('background', theme_base)
+end
+
 return {
   'joshdick/onedark.vim', -- Theme inspired by Atom
   {
     'overcache/NeoSolarized',
+    requires = {
+      'rktjmp/fwatch.nvim',
+    },
     config = function()
-      local handle = io.popen 'current-base16'
-      local result = handle:read '*a'
-      local theme_base = (result:gsub('solarized--', ''))
-      handle:close()
+      -- set a theme first
+      vim.cmd [[ colorscheme NeoSolarized ]]
 
-      -- -- although below "works" it doesn't work well with my cached base16 shell eval - look at the end of ./shell_ext
-      -- local theme_base = (vim.env.BASE16_THEME:gsub('solarized--', ''))
+      -- comply with base16
+      Follow_base16_shell()
 
-      vim.cmd([[
-  colorscheme NeoSolarized
-  set background=]] .. theme_base)
+      -- set up a callback on file change so it can correct the theme tone by itself
+      local fwatch = require 'fwatch'
+      -- because `~` wouldn't work here
+      fwatch.watch(vim.env.HOME .. '/.base16_theme.updated-time', {
+        on_event = function()
+          -- use `defer_fn` to avoid [blahblah] must not be called in a lua loop callback
+          vim.defer_fn(function()
+            Follow_base16_shell()
+          end, 1)
+        end,
+      })
     end,
   },
   -- {
