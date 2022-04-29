ec91cd39e7cc1491f5bef44083c123ea36b08929
Heechul Ryu <ryuhcii@gmail.com>
2022-04-29 10:01:56 -0700

    bin - compatibility fixes
    - discoverable/single-daemon
      - use $HOME/tmp instead of /var/tmp for this to be portable across different user accounts on macOS
    - darwin/watch-appearance-changes.sh
      - use `timeout` to group processes separately

--- bin/darwin/watch-appearance-changes.sh
+++ bin/darwin/watch-appearance-changes.sh
@@ -4,5 +4,15 @@
 # by copying the command below
 # ~/.config/dfs-rhc/bin/darwin/watch-appearance-changes.sh
 
+# `timeout` is a part of GNU Coreutils - https://www.gnu.org/software/coreutils/manual/coreutils.html#timeout-invocation
+# Without it, all of the commands in this script will be in the same group - meaning killing one (by `single-daemon`) will kill all
+# Which is a undesired behavior with `single-daemon`
+# therefore use timeout to have a side effect of having separate process group id
+# This method was discovered at https://unix.stackexchange.com/a/403193/396504
+
 export FORCE_LOAD_MY_ZSH_STUFF=1
-zsh -lc 'source ~/.zshrc; single-daemon base16-shell-auto-reload-on-tmux; single-daemon ~/.config/dfs-rhc/bin/darwin/base16-shell-to-follow-system-appearance.sh'
+exec zsh -lc '
+source ~/.zshrc
+timeout 5s single-daemon base16-shell-auto-reload-on-tmux
+timeout 5s single-daemon ~/.config/dfs-rhc/bin/darwin/base16-shell-to-follow-system-appearance.sh
+'
