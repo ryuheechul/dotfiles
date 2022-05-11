d46156865a14ccd5724529c29f898345a8650f36
Heechul Ryu <ryuhcii@gmail.com>
2022-05-11 12:17:36 -0700

    emacs - add "activate" option to recognize restarted Emacs instances
    as-was: it only tries to open and fail to activate with restarted
    instances and instead it opens another instance which is not desired
    new behavior: if running instance is found, activate rather than opening
    a new one

--- bin/darwin/open-gui-emacs.sh
+++ bin/darwin/open-gui-emacs.sh
@@ -2,5 +2,12 @@
 
 # this script can be called from an Automator app to mitigate https://github.com/nix-community/home-manager/issues/1341
 # and put the (Automator) app in either /Applications or ~/Applications so either Spotlight or Alfred can open (or activate the (Emacs) app quickly
+# Also avoid naming the Automator app "Emacs.app" to prevent collision with "~/.nix-profile/Applications/Emacs.app" on `"Emacs" to activate` Applescript side - For example, name it to "OpenEmacs.app"
 
-open -a ~/.nix-profile/Applications/Emacs.app
+osascript -e 'if application "Emacs" is not running then
+	error "emacs is not running"
+end if' 2> /dev/null && {
+	osascript -e 'tell application "Emacs" to activate'
+} || {
+	open -a ~/.nix-profile/Applications/Emacs.app
+}
