aa1b8332638ddb8d02e4abbca74ba5848aac0776
Heechul Ryu <ryuhcii@gmail.com>
2022-06-10 16:29:37 -0700

    nix - add more erlang packages
    - erlang, rebar3, gleam
    - also add gleam syntax to bat

--- nix/pkgs/extra/default.nix
+++ nix/pkgs/extra/default.nix
@@ -48,7 +48,10 @@ in
   ]
   ++ lib.optionals (checkEnv "MY_NIX_EXTRA_ERLANG")
   [
+    erlang # Programming language used for massively scalable soft real-time systems
+    rebar3 # Erlang build tool that makes it easy to compile and test Erlang applications, port drivers and releases
     elixir # A functional, meta-programming aware language built on top of the Erlang VM
+    gleam # A statically typed language for the Erlang VM
   ]
   ++ lib.optionals (checkEnv "MY_NIX_EXTRA_EXERCISM")
   [
