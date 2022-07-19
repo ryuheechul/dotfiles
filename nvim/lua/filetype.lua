b6e7640a9178e03555fe192cb496649fc11a9fe7
Heechul Ryu <ryuhcii@gmail.com>
2022-07-18 17:12:11 -0700

    nvim - add `source .env` snippet

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -4,6 +4,7 @@ vim.cmd [[
 autocmd FileType markdown setlocal shiftwidth=2 softtabstop=2 expandtab
 autocmd FileType typescript setlocal shiftwidth=2 softtabstop=2 expandtab
 autocmd FileType Earthfile setlocal shiftwidth=2 softtabstop=2 expandtab
+autocmd FileType json setlocal shiftwidth=2 softtabstop=2 expandtab
 ]]
 
 -- vim: ts=2 sts=2 sw=2 et
