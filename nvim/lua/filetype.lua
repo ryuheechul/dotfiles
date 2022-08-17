420fa19c53a0c9ee265e6ccfc96a869f8beae829
Heechul Ryu <ryuhcii@gmail.com>
2022-08-16 18:00:55 -0700

    nvim - set tabstop for javascript

--- nvim/lua/filetype.lua
+++ nvim/lua/filetype.lua
@@ -3,6 +3,7 @@
 vim.cmd [[
 autocmd FileType markdown setlocal shiftwidth=2 softtabstop=2 expandtab
 autocmd FileType typescript setlocal shiftwidth=2 softtabstop=2 expandtab
+autocmd FileType javascript setlocal shiftwidth=2 softtabstop=2 expandtab
 autocmd FileType Earthfile setlocal shiftwidth=2 softtabstop=2 expandtab
 autocmd FileType json setlocal shiftwidth=2 softtabstop=2 expandtab
 ]]
