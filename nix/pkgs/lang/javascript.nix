50979a78038c017fb0006b13b4e53006df33666d
Heechul Ryu <ryuhcii@gmail.com>
2022-10-01 18:18:29 -0700

    nix - add yalc package

--- nix/pkgs/lang/javascript.nix
+++ nix/pkgs/lang/javascript.nix
@@ -2,21 +2,28 @@
 
 with pkgs;
 let
-  # deno - i should move this extra
   bundle-deno = [ deno ];
-  # nodejs
   nodejs = nodejs-18_x;
   bundle-nodejs-with-pkgs = (
     # node essentials
     [
+      # Event-driven I/O framework for the V8 JavaScript engine
       nodejs
+      # Fast, reliable, and secure dependency management for javascript
       yarn
       # npm packages via nodePackages
     ] ++ (with nodePackages; [
+      # Generate Nix expressions to build NPM packages
       node2nix
+      # Fast, disk space efficient package manager
       pnpm
-      pyright # lsp server for python
+      # Work with npm/yarn packages locally like a boss.
+      yalc
+      # lsp server for python
+      pyright
+      # TypeScript is a language for application scale JavaScript development
       typescript
+      # Language Server Protocol (LSP) implementation for TypeScript using tsserver
       typescript-language-server
       # for https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#eslint
       vscode-langservers-extracted
