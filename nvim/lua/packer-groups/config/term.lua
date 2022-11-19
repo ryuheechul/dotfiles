05be9982fc305769faddd93915eb4199b45c6897
Heechul Ryu <ryuhcii@gmail.com>
2022-11-18 21:07:40 -0800

    nvim - move (toggle)term config to its own file

---
+++ nvim/lua/packer-groups/config/term.lua
@@ -0,0 +1,62 @@
+-- term settings
+
+return function()
+  -- augment terminal
+  vim.cmd [[
+        " cleaner approach than $FORCE_LOAD_MY_ZSH_STUFF
+        let $UNSET_MY_BASIC_ZSH_STUFF_LOADED=1
+        let $UNSET_ALL_MY_ZSH_STUFF_LOADED=1
+
+        let $NO_VI_KEY_ON_ZSH = 1
+      ]]
+
+  require('toggleterm').setup {
+    -- size can be a number or function which is passed the current terminal
+    size = function(term)
+      if term.direction == 'horizontal' then
+        return 15
+      elseif term.direction == 'vertical' then
+        return vim.o.columns * 0.4
+      end
+    end, -- | 20
+    open_mapping = [[<c-\>]],
+    hide_numbers = true, -- hide the number column in toggleterm buffers
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+    start_in_insert = true,
+    insert_mappings = true, -- whether or not the open mapping applies in insert mode
+    persist_size = true,
+    direction = 'horizontal', -- | 'vertical' | 'window' | 'float',
+    close_on_exit = true, -- close the terminal window when the process exits
+    shell = vim.o.shell, -- change the default shell
+    -- This field is only relevant if direction is set to 'float'
+    float_opts = {
+      -- The border key is *almost* the same as 'nvim_win_open'
+      -- see :h nvim_win_open for details on borders however
+      -- the 'curved' border is a custom border type
+      -- not natively supported but implemented in this plugin.
+      border = 'single', -- | 'double' | 'shadow' | 'curved' | ... other options supported by win open
+      -- width = <value>,
+      -- height = <value>,
+      winblend = 3,
+      highlights = {
+        border = 'Normal',
+        background = 'Normal',
+      },
+    },
+  }
+
+  function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  end
+
+  -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+  vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
+end
--- nvim/lua/packer-groups/system.lua
+++ nvim/lua/packer-groups/system.lua
@@ -6,69 +6,9 @@ return {
   'christoomey/vim-tmux-navigator', -- navigate with tmux key binding
   {
     'akinsho/toggleterm.nvim', -- a great ergonomic terminal customization
-    config = function()
-      -- augment terminal
-      vim.cmd [[
-        " cleaner approach than $FORCE_LOAD_MY_ZSH_STUFF
-        let $UNSET_MY_BASIC_ZSH_STUFF_LOADED=1
-        let $UNSET_ALL_MY_ZSH_STUFF_LOADED=1
-
-        let $NO_VI_KEY_ON_ZSH = 1
-      ]]
-
-      require('toggleterm').setup {
-        -- size can be a number or function which is passed the current terminal
-        size = function(term)
-          if term.direction == 'horizontal' then
-            return 15
-          elseif term.direction == 'vertical' then
-            return vim.o.columns * 0.4
-          end
-        end, -- | 20
-        open_mapping = [[<c-\>]],
-        hide_numbers = true, -- hide the number column in toggleterm buffers
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-        start_in_insert = true,
-        insert_mappings = true, -- whether or not the open mapping applies in insert mode
-        persist_size = true,
-        direction = 'horizontal', -- | 'vertical' | 'window' | 'float',
-        close_on_exit = true, -- close the terminal window when the process exits
-        shell = vim.o.shell, -- change the default shell
-        -- This field is only relevant if direction is set to 'float'
-        float_opts = {
-          -- The border key is *almost* the same as 'nvim_win_open'
-          -- see :h nvim_win_open for details on borders however
-          -- the 'curved' border is a custom border type
-          -- not natively supported but implemented in this plugin.
-          border = 'single', -- | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-          -- width = <value>,
-          -- height = <value>,
-          winblend = 3,
-          highlights = {
-            border = 'Normal',
-            background = 'Normal',
-          },
-        },
-      }
-
-      function _G.set_terminal_keymaps()
-        local opts = { buffer = 0 }
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-        vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-      end
-
-      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-      vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
-    end,
+    config = require 'packer-groups.config.term',
   },
-  -- UI to select things (files, grep results, open buffers...)
-  {
+  { -- UI to select things (files, grep results, open buffers...)
     'nvim-telescope/telescope.nvim',
     requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
     config = function()
@@ -85,7 +25,7 @@ return {
       }
     end,
   },
-  {
+  { -- A file explorer tree for neovim written in lua
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
     config = function()
@@ -113,13 +53,13 @@ return {
       }
     end,
   }, -- enhanced filetree replacing netrw
-  {
+  { -- https://github.com/fregante/GhostText
     'subnut/nvim-ghost.nvim',
     run = ':call nvim_ghost#installer#install()',
     cond = function()
       return vim.env.my_nvim_ghost ~= nil
     end,
-  }, -- https://github.com/fregante/GhostText
+  },
   { -- A snazzy bufferline for Neovim - an upgrade from 'ap/vim-buftabline'
     'akinsho/bufferline.nvim',
     tag = 'v3.*',
@@ -133,7 +73,7 @@ return {
       }
     end,
   },
-  {
+  { -- A blazing fast and easy to configure Neovim statusline written in Lua
     'nvim-lualine/lualine.nvim',
     requires = {
       'kyazdani42/nvim-web-devicons',
