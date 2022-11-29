24e0f2f42bc55a25890603393f3b8ba33d860b3a
Heechul Ryu <ryuhcii@gmail.com>
2022-11-29 14:43:31 -0800

    nvim - use profile.nvim

--- nvim/lua/packer-groups/system.lua
+++ nvim/lua/packer-groups/system.lua
@@ -128,6 +128,40 @@ return {
     },
     config = require 'packer-groups.config.bars',
   },
+  { -- A lua profiler for neovim that is discovered thanks to https://www.reddit.com/r/neovim/comments/xicxox/comment/ip2hprd
+    'stevearc/profile.nvim',
+    config = function()
+      local should_profile = os.getenv 'NVIM_PROFILE'
+      if should_profile then
+        require('profile').instrument_autocmds()
+        if should_profile:lower():match '^start' then
+          require('profile').start '*'
+        else
+          require('profile').instrument '*'
+        end
+      end
+
+      local function toggle_profile()
+        local prof = require 'profile'
+        if prof.is_recording() then
+          prof.stop()
+          vim.ui.input(
+            { prompt = 'Save profile to:', completion = 'file', default = 'profile.json' },
+            function(filename)
+              if filename then
+                prof.export(filename)
+                vim.notify(string.format('Wrote %s', filename))
+              end
+            end
+          )
+        else
+          prof.start '*'
+        end
+      end
+
+      vim.keymap.set('', '<f1>', toggle_profile)
+    end,
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
