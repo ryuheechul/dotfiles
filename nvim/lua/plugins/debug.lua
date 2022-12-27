9bab654e6f5fb6d6eaa98aa920598ad62a6df29c
Heechul Ryu <ryuhcii@gmail.com>
2022-12-26 20:51:52 -0800

    nvim - lazify plugins/debug.lua

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -1,26 +1,24 @@
 -- language debugging tools
 
 return {
-  'rafcamlet/nvim-luapad', -- `:Luapad` to open a pad and now easily see the result of code in the pad
-  {
-    'folke/trouble.nvim', -- `:Trouble` to show current troubles in the buffer
+  { -- `:Luapad` to open a pad and now easily see the result of code in the pad
+    'rafcamlet/nvim-luapad',
+    event = 'VeryLazy',
+  },
+  { -- `:Trouble` to show current troubles in the buffer
+    'folke/trouble.nvim',
     -- these are actually optional but I require to get benefits of them
     dependencies = {
       'nvim-tree/nvim-web-devicons',
       'folke/lsp-colors.nvim',
     },
-    config = function()
-      require('trouble').setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end,
+    config = true,
+    event = 'VeryLazy',
   },
-  {
+  { -- Sniprun is a code runner plugin for neovim written in Lua and Rust
     'michaelb/sniprun',
     build = 'bash ./install.sh',
-    lazy = true,
+    event = 'VeryLazy',
     -- until there is more update on https://github.com/michaelb/sniprun/issues/54
     cond = function()
       return vim.fn.has 'linux' == 1
@@ -28,12 +26,13 @@ return {
   },
   -- maybe one day this becomes useful
   -- { 'rcarriga/vim-ultest', dependencies = { 'vim-test/vim-test' }, build = ':UpdateRemotePlugins' },
-  {
-    'weilbith/nvim-code-action-menu', -- enables `:CodeActionMenu`
+  { -- enables `:CodeActionMenu`
+    'weilbith/nvim-code-action-menu',
     cmd = 'CodeActionMenu',
   },
   { -- Debug Adapter Protocol client implementation for Neovim
     'mfussenegger/nvim-dap',
+    event = 'VeryLazy',
     dependencies = {
       'anuvyklack/keymap-layer.nvim', -- Create a key layer in Neovim
       'jbyuki/one-small-step-for-vimkind', -- Debug adapter for Neovim lua files (including plugins)
