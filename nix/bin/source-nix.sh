85558c144a45c3726f391e07ff86817284c9a565
ryuhcii@gmail.com <ryucii@gmail.com>
2023-01-13 19:17:47 -0800

    bootstrap - add sourcing nix to ~/.bashrc

--- nix/bin/source-nix.sh
+++ nix/bin/source-nix.sh
@@ -1,23 +1,23 @@
 # This file is expected to be sourced not executed
 
-function safe-source() {
+function safe_source() {
   test -f "${1}" && source "${1}"
 }
 
 # try standard linux way first
 if test -z "$(command -v nix)"; then
-  safe-source /etc/profile.d/nix.sh
-  safe-source /etc/profile.d/user-shim-for-nix-path.sh
+  safe_source /etc/profile.d/nix.sh
+  safe_source /etc/profile.d/user-shim-for-nix-path.sh
 fi
 
 # try another way next
 if test -z "$(command -v nix)"; then
-  safe-source ~/.nix-profile/etc/profile.d/nix.sh
+  safe_source ~/.nix-profile/etc/profile.d/nix.sh
 fi
 
 # try another way next
 if test -z "$(command -v nix)"; then
-  safe-source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  safe_source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
 if test -z "$(command -v nix)"; then
