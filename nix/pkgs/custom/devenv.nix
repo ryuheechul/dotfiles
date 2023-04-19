cd48af3fbf73fe6a05ad2eafb9f6421716af7573
Heechul Ryu <ryuhcii@gmail.com>
2023-04-19 14:14:48 -0700

    nix - upgrade devenv to 0.6.2

--- nix/pkgs/custom/devenv.nix
+++ nix/pkgs/custom/devenv.nix
@@ -1,6 +1,6 @@
 # https://devenv.sh/getting-started/#__tabbed_3_3
 let
-  version = "0.5";
+  version = "0.6.2";
   url = "https://github.com/cachix/devenv/archive/v${version}.tar.gz";
 in
 (import (fetchTarball url)).default
--- nix/pkgs/custom/src-cli.nix
+++ nix/pkgs/custom/src-cli.nix
@@ -1,6 +1,9 @@
 let
-  rev = "0c80e917b50eb934c9324e44961538e0826a6f58";
   # until this merges to upstream, https://github.com/sourcegraph/src-cli
+  rev = "nix";
   url = "https://github.com/ryuheechul/src-cli/archive/${rev}.tar.gz";
+  tarball = fetchTarball url;
+  at-contrib = tarball + "/contrib";
 in
-(import (fetchTarball url)).default
+(import at-contrib).default
+# NOTE: the binary is `src` not `src-cli`
