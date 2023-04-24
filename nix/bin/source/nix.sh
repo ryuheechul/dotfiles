fb3691b149057a925831f18c5d6b5ec3d0a9fcaf
Heechul Ryu <ryuhcii@gmail.com>
2023-04-24 09:44:11 -0700

    zsh - add alias to help debug home-manager and bash

--- nix/bin/source/nix.sh
+++ nix/bin/source/nix.sh
@@ -5,21 +5,21 @@ function safe_source() {
 }
 
 # try standard linux way first
-if test -z "$(command -v nix)"; then
+if ! command -v nix > /dev/null; then
   safe_source /etc/profile.d/nix.sh
   safe_source /etc/profile.d/user-shim-for-nix-path.sh
 fi
 
 # try another way next
-if test -z "$(command -v nix)"; then
+if ! command -v nix > /dev/null; then
   safe_source ~/.nix-profile/etc/profile.d/nix.sh
 fi
 
 # try another way next
-if test -z "$(command -v nix)"; then
+if ! command -v nix > /dev/null; then
   safe_source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
 
-if test -z "$(command -v nix)"; then
+if ! command -v nix > /dev/null; then
   echo 'Warning: `nix` is still not found'
 fi
