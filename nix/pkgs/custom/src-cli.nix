a30b06c6da375a5039e033a03ea3eb611315063a
Heechul Ryu <ryuhcii@gmail.com>
2023-04-24 09:07:58 -0700

    nix - update src-cli to upstream

--- nix/pkgs/custom/src-cli.nix
+++ nix/pkgs/custom/src-cli.nix
@@ -1,7 +1,7 @@
 let
-  # until this merges to upstream, https://github.com/sourcegraph/src-cli
-  rev = "nix";
-  url = "https://github.com/ryuheechul/src-cli/archive/${rev}.tar.gz";
+  # TODO: change this to a version tag when new release come out
+  rev = "1ef7956e2de5f0aea295631f98d6f0326c2b6367";
+  url = "https://github.com/sourcegraph/src-cli/archive/${rev}.tar.gz";
   tarball = fetchTarball url;
   at-contrib = tarball + "/contrib";
 in
