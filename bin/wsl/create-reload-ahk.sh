2e3810accdc4b554dfd14600ce2b4e5ff144428a
Heechul Ryu <ryuhcii@gmail.com>
2023-04-28 22:20:30 -0700

    wsl - fix bin/wsl/create-reload-ahk.sh to actually work

--- bin/wsl/create-reload-ahk.sh
+++ bin/wsl/create-reload-ahk.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
+WND_USERNAME="$(powershell.exe '$env:UserName' | sed 's/.$//')" # removing mysterious extra character at the end with sed
+
 if [ -z "${WND_USERNAME}" ]; then
   echo 'Exiting because no $WND_USERNAME is set'
   echo "Try again like WND_USERNAME=my-wnd-user $(readlink -f $0)"
   exit 1
 fi
 
-target_path="/mnt/c/Users/${WND_USERNAME}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/reload-ahk.sh"
+target_dir="/mnt/c/Users/${WND_USERNAME}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+target_path_for_win="$(wslpath -w "${target_dir}")\my-hotkeys.ahk.lnk"
 
-cat <<EOF > "${target_path}"
-#!/usr/bin/env bash
+to_link="$(wslpath -w "${my_dot_d}/autohotkey/my-hotkeys.ahk")"
 
-cp ~/dotfiles/autohotkey/my-hotkeys.ahk ./
+cmd_to_run="powershell.exe 'New-Item -ItemType SymbolicLink -Path \"${target_path_for_win}\" -Target \"${to_link}\"'"
 
-/mnt/c/Program\ Files/AutoHotkey/AutoHotkey.exe my-hotkeys.ahk
-EOF
+wudo="python3 ${HOME}/.wsl-sudo/wsl-sudo.py"
 
-chmod +x "${target_path}"
+${wudo} bash -c "${cmd_to_run}"
 
-echo "Now you can run \`${target_path}\` to easily reload hot keys."
-echo "Also \`~/dotfiles/autohotkey/my-hotkeys.ahk\` will be loaded on start up as well."
-echo "If this doesn't work creating a shortcut manually and place it it Startup folder also should work"
+echo "Now \`${target_path_for_win}\` will be loaded on start up"
+echo "which is linked to ${to_link}"
