af47e9440314e50e8711654e4bb8308f8d5cdeb6
Heechul Ryu <ryuhcii@gmail.com>
2023-07-05 21:49:33 -0700

    nix - add nmap to pkgs/linux.nix

--- nix/pkgs/linux.nix
+++ nix/pkgs/linux.nix
@@ -8,5 +8,6 @@ with pkgs;(
       sysstat # A collection of performance monitoring tools for Linux (such as sar, iostat and pidstat)
       traceroute # Tracks the route taken by packets over an IP network
       unixtools.netstat
+      nmap # A free and open source utility for network discovery and security auditing
     ]
 )
