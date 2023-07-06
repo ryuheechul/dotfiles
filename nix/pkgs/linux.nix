583e0f979a617b9914d2bede3e0a687f1915a81d
Heechul Ryu <ryuhcii@gmail.com>
2023-07-05 21:19:46 -0700

    nix - add netstat for pkgs/linux.nix

--- nix/pkgs/linux.nix
+++ nix/pkgs/linux.nix
@@ -7,5 +7,6 @@ with pkgs;(
       iotop # A tool to find out the processes doing the most IO
       sysstat # A collection of performance monitoring tools for Linux (such as sar, iostat and pidstat)
       traceroute # Tracks the route taken by packets over an IP network
+      unixtools.netstat
     ]
 )
