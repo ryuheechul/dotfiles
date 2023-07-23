e0ddf91faf48a3b6dd745efbf56dec7277e93d1f
Heechul Ryu <ryuhcii@gmail.com>
2023-07-22 22:37:39 -0700

    tmux - replace battery glyphs to follow new nerd-fonts

--- nix/pkgs/fonts/default.nix
+++ nix/pkgs/fonts/default.nix
@@ -3,6 +3,9 @@
 # the actual linking is delegated to ../../home/default.nix
 
 let
+  # run https://github.com/ryanoasis/nerd-fonts/blob/master/bin/scripts/test-fonts.sh \
+  # to view and test fonts
+  # (visit the URL to see how-to)
   fonts_for_nerds = [ "FiraMono" "JetBrainsMono" ];
   # If Terminal app is installed on Windows host (e.g. Windows native Alacritty) not on WSL,
   # home-manager running within WSL won't cover installing fonts to Windows host.
