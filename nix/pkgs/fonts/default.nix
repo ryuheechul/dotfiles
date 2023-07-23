b908ffcee39c6f64e4030c65abed174725431f67
Heechul Ryu <ryuhcii@gmail.com>
2023-07-22 22:48:43 -0700

    tmux - update the hud to show the battery on the right side

--- nix/pkgs/fonts/default.nix
+++ nix/pkgs/fonts/default.nix
@@ -4,8 +4,9 @@
 
 let
   # run https://github.com/ryanoasis/nerd-fonts/blob/master/bin/scripts/test-fonts.sh \
-  # to view and test fonts
-  # (visit the URL to see how-to)
+  # to view and test fonts (visit the URL above to see how-to)
+  # and watch if watch [Effective Nerd Fonts in Multiple Terminals](https://www.youtube.com/watch?v=mQdB_kHyZn8)
+  # to get a feel about how to configure fonts for different terminals and linux (for fallbacks)
   fonts_for_nerds = [ "FiraMono" "JetBrainsMono" ];
   # If Terminal app is installed on Windows host (e.g. Windows native Alacritty) not on WSL,
   # home-manager running within WSL won't cover installing fonts to Windows host.
