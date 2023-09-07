1cdd34e91b240b90fefc44bfebf8401a92167739
Heechul Ryu <ryuhcii@gmail.com>
2023-09-07 11:21:45 -0700

    nix - bump devenv to 0.6.3

--- nix/pkgs/custom/devenv.nix
+++ nix/pkgs/custom/devenv.nix
@@ -1,6 +1,6 @@
 # https://devenv.sh/getting-started/#__tabbed_3_3
 let
-  version = "0.6.2";
+  version = "0.6.3";
   url = "https://github.com/cachix/devenv/archive/v${version}.tar.gz";
 in
 (import (fetchTarball url)).default
