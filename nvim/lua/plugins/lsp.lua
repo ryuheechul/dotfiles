6845b4ff19501afb55a4da53d1bef4c3a632c503
Heechul Ryu <ryuhcii@gmail.com>
2023-10-06 11:56:36 -0700

    nvim - update sg.nvim plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -28,11 +28,10 @@ return {
     config = require('plugins.config.lsp').null_ls,
   },
   { --  bringing many of the features of sourcegraph.com into Neovim
-    'tjdevries/sg.nvim', -- this is technically not directly related to lsp but it uses lsp so it can stay in this file for now.
+    'sourcegraph/sg.nvim', -- this is technically not directly related to lsp but it uses lsp so it can stay in this file for now.
     -- try `nvim sg://github.com/ryuheechul/hexto256/-/main.go` from shell
     -- or `:edit sg://github.com/ryuheechul/hexto256/-/main.go` inside neovim
     -- I also made a zsh alias so it can be used like this, `sggh ryuheechul/hexto256 main.go`
-    build = "np-build-via-nix-shell 'cargo build --workspace'",
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = true,
     cond = vim.env.my_nvim_use_sg ~= nil,
