96842e8bf24d5e7a4f1ca3efcfa13efa4b3a53fc
Heechul Ryu <ryuhcii@gmail.com>
2024-01-21 12:13:00 -0800

    nixos - add note regarding group changes for a user

--- nix/nixos/user.nix
+++ nix/nixos/user.nix
@@ -8,6 +8,10 @@ in
 with pkgs.lib;{
   isNormalUser = mkForce true;
   description = mkForce "user is ${username}";
+  # NOTE regarding groups:
+  # - changes on groups can be seen right away with `cat /etc/group` but not with `groups`
+  # - this will take affect at login time which may require rebooting
+  # - https://stackoverflow.com/a/7537275/1570165
   extraGroups = mkForce ([ "networkmanager" "wheel" ] ++ groups);
   packages = mkMerge [ ]; # since I already manage my packages via ../pkgs
   # if want a package that is NixOS specific look at ./system-pkgs.nix
