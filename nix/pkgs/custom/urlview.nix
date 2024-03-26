f57587e3ba63bc6b08fd8708cdf5fbd38ea3e29b
Heechul Ryu <ryuhcii@gmail.com>
2024-03-26 07:37:09 -0700

    bin - ssh/xdg-open improve experience with urlscan

--- nix/pkgs/custom/urlview.nix
+++ nix/pkgs/custom/urlview.nix
@@ -3,6 +3,21 @@
 # obviously this is actually urlscan not urlview but i'm using it in place anyway
 # ../../../urlscan to view/edit config
 # fake the name until https://github.com/tmux-plugins/tmux-urlview/issues/29 gets resolved
-pkgs.writeShellScriptBin "urlview" ''
-  ${pkgs.urlscan}/bin/urlscan -s "$@"
-''
+let
+  wrapped = pkgs.writeShellScriptBin "urlview" ''
+    exec ${pkgs.urlscan}/bin/urlscan -s
+  '';
+in
+pkgs.symlinkJoin {
+  name = "urlview";
+  paths = [
+    wrapped
+    pkgs.urlscan
+  ];
+}
+
+# favoring above instead of below according to https://nixos.wiki/wiki/Nix_Cookbook#Wrapping_packages
+
+# pkgs.writeShellScriptBin "urlview" ''
+#   exec ${pkgs.urlscan}/bin/urlscan -s "$@"
+# ''
