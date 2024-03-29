bed10c13240684507755f7fa7bb58570b56e873e
Heechul Ryu <ryuhcii@gmail.com>
2024-03-29 12:08:17 -0700

    kitty - set font and size

--- nix/home/programs/terminals.nix
+++ nix/home/programs/terminals.nix
@@ -19,9 +19,16 @@ in
     # https://nix-community.github.io/home-manager/options.html#opt-programs.kitty.enable
     enable = true;
 
+    font = {
+      name = "FiraMono Nerd Font Mono";
+      size = 15;
+    };
+
     settings = {
       hide_window_decorations = true;
       startup_session = startup_session_path;
+      # unfortunately I couldn't find a way to set full screen startup in the configuration - https://github.com/kovidgoyal/kitty/issues/856
+      # in the meantime, run it with `kitty --start-as=fullscreen` from other terminal or need to create a .desktop file to that.
     };
   };
 }
