b997112d7f72b2192f4b7db84f0bb75bb58089ac
Heechul Ryu <ryuhcii@gmail.com>
2024-05-27 22:35:39 -0700

    nixos - add comments on local usage for pam-sshagent.nix

--- nix/nixos/recipes/pam-sshagent.nix
+++ nix/nixos/recipes/pam-sshagent.nix
@@ -10,4 +10,10 @@
       "/etc/ssh/authorized_keys.d/%u"
     ];
   };
+
+  # (with 1password) why this doesn't work locally but remotely (if remote host configured that)
+  # - it's explained at https://developer.1password.com/docs/ssh/agent/compatibility/
+  # - the gist is 1password guides you to setup `IdentityAgent` at `~/.ssh/config` instead of `SSH_AUTH_SOCK` environment variable
+  # - set `test -S ~/.1password/agent.sock && export SSH_AUTH_SOCK=~/.1password/agent.sock` in your shell will resolve the issue
+  # - furthermore `IdentityAgent` will interfere with client's `SSH_AUTH_SOCK` so just sticking with `SSH_AUTH_SOCK` seems a smoother experience overall
 }
