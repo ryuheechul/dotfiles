5bc597eb132cd2d09ca1b441830375e31d379af1
Heechul Ryu <ryuhcii@gmail.com>
2024-05-27 20:03:09 -0700

    nixos - improve gen-local-only.sh

--- nix/nixos/gen-local-only.sh
+++ nix/nixos/gen-local-only.sh
@@ -2,10 +2,12 @@
 
 script_d="$(dirname "$0")"
 
-cat <<EOF >> "${script_d}/system-pkgs-local.nix"
+test -f "${script_d}/system-pkgs-local.nix" || cat <<EOF >> "${script_d}/system-pkgs-local.nix"
 { pkgs }:
 
 with pkgs;
 [
 ]
 EOF
+
+test -f "${script_d}/mix-and-match.nix" || cp "${script_d}/mix-and-match-template.nix" "${script_d}/mix-and-match.nix"
