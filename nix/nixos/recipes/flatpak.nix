2503d886702df5491757c8474cab8ea8ea7ff9ab
Heechul Ryu <ryuhcii@gmail.com>
2024-10-28 11:25:10 -0700

    nixos - add io.github.plrigaux.sysd-manager via flatpak
    and remove the wrong import line in recipes/flatpak.nix

--- nix/nixos/recipes/flatpak.nix
+++ nix/nixos/recipes/flatpak.nix
@@ -1,6 +1,5 @@
 { ... }:
 
-
 # https://nixos.wiki/wiki/Flatpak
 # https://nixos.org/manual/nixos/stable/index.html#module-services-flatpak
 # https://flatpak.org/setup/NixOS
@@ -8,11 +7,9 @@
 # `flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo`
 # `export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share`
 {
-  imports =
-    [
-      <nixos-hardware/gpd/win-max-2>
-      ../../pkgs/custom/nix-flatpak.nix # to import https://github.com/gmodena/nix-flatpak module
-    ];
+  imports = [
+    ../../pkgs/custom/nix-flatpak.nix # to import https://github.com/gmodena/nix-flatpak module
+  ];
 
   services.flatpak.enable = true;
 
@@ -22,7 +19,7 @@
     "com.google.Chrome" # Google Chrome
     "io.github.arunsivaramanneo.GPUViewer" # GPU-Viewer
     "io.github.zen_browser.zen" # Zen
-
+    "io.github.plrigaux.sysd-manager" # a GUI to manage systemd units
     # "net.lutris.Lutris" # Lutris
     # "com.slack.Slack" # Slack
   ];
