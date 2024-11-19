5afa9ee96ae58722c572a7c087186373b289d7a2
Heechul Ryu <ryuhcii@gmail.com>
2024-11-18 21:10:20 -0800

    nix - replace neofetch with fastfetch

--- nix/pkgs/default.nix
+++ nix/pkgs/default.nix
@@ -12,7 +12,8 @@ with pkgs;
   hello
   cowsay
   fortune
-  neofetch
+  fastfetch # An actively maintained, feature-rich and performance oriented, neofetch like system information tool
+  ipfetch # like Neofetch but for ip addresses
 ]
 ++
   # linuxify - enhancing linux compatibility
