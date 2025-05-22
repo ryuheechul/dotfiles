e256ed1814c58c9cb367a0b3d0f487a81f1cea29
Heechul Ryu <ryuhcii@gmail.com>
2025-05-21 20:43:31 -0700

    nixos - recipes/tailscale.nix - use routing

--- nix/nixos/recipes/tailscale.nix
+++ nix/nixos/recipes/tailscale.nix
@@ -7,10 +7,13 @@
   ];
 
   services.tailscale.enable = true;
+  services.tailscale.useRoutingFeatures = "both";
 
   # if using exit node or subnet routing;
   # https://discourse.nixos.org/t/tailscale-exit-node-not-working-on-nixos/39897
   # set it like this somewhere `services.tailscale.useRoutingFeatures = "both"`; # or "client" or "server" or none
   # `nixos-option services.tailscale.useRoutingFeatures` to read more about and;
   # see this for the detailed behavior https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/services/networking/tailscale.nix
+
+  # WARN: watch out for the issue with 6.11.x kernel - https://github.com/tailscale/tailscale/issues/13863
 }
