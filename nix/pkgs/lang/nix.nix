97de235168281569282f8e49819b9bae695f7ed7
Heechul Ryu <ryuhcii@gmail.com>
2026-03-17 15:25:42 -0700

    nix - add comma that works like `nix-shell -p`

--- nix/pkgs/lang/nix.nix
+++ nix/pkgs/lang/nix.nix
@@ -7,6 +7,7 @@ with pkgs;
   # nixpkgs-fmt # Nix code formatter for nixpkgs - deprecated and now there is an official formatter below
   nixfmt-rfc-style # Official formatter for Nix code
   niv # Easy dependency management for Nix projects
+  comma # Runs programs without installing them
 ]
 
 # regarding niv:
