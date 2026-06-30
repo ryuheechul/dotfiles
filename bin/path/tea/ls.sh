68257d21213e734785da8cd9f3963f6d4e57d64c
Heechul Ryu <ryuhcii@gmail.com>
2026-06-29 18:09:04 -0700

    tea, yarn - remove integrations
    mise replaces what tea was supposed to supplement nixpkgs

--- bin/path/tea/ls.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-script_d="$(dirname "$0")"
-
-exa "${script_d}/bin/" -l
--- nix/pkgs/lang/rust.nix
+++ nix/pkgs/lang/rust.nix
@@ -5,8 +5,7 @@ with pkgs;
   evcxr # An evaluation context for Rust
   ## - I found that using devenv is the most smooth experience with Rust via https://nixos.wiki/wiki/Rust on Nix
   ## - An alternative to uncomment packages below would be
-  ##   1. use `export MY_NIX_EXTRA_DEVENV=1` instead and configure per project via `devenv init`
-  ##   2. or read ../../../bin/path/tea/README.md to use tea
+  ##   use `export MY_NIX_EXTRA_DEVENV=1` instead and configure per project via `devenv init`
   # cargo
   # rustc
 ]
